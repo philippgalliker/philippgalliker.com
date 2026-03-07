cff5ec0b0958a91ae6841be65ccbc7567c849104
Philipp Galliker <philipp.galliker88@gmail.com>
2026-03-07 09:23:38 +0100

    Refactor footer layout for improved responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,12 +19,16 @@ export default function Footer() {
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
           <SocialIcon kind="medium" href={siteMetadata.medium} size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.footerTitle}</Link>
+        <div className="mb-2 flex flex-col items-center text-sm text-gray-500 sm:flex-row sm:space-x-2 dark:text-gray-400">
+          <div className="flex space-x-2">
+            <div>{siteMetadata.author}</div>
+            <div>{` • `}</div>
+            <div>{`© ${new Date().getFullYear()}`}</div>
+          </div>
+          <div className="hidden sm:block">{` • `}</div>
+          <Link className="break-words" href="/">
+            {siteMetadata.footerTitle}
+          </Link>
         </div>
       </div>
     </footer>
