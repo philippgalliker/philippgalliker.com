aae08175fef1ab4e4126246cc0c0e9f703716cd2
Philipp Galliker <philipp.galliker88@gmail.com>
2026-07-09 11:48:55 +0200

    Add LinkedIn profile link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
   // twitter: 'https://twitter.com/Twitter',
   //facebook: 'https://facebook.com',
   //youtube: 'https://youtube.com',
-  //linkedin: 'https://www.linkedin.com/in/philipp-galliker-37283b335/',
+  linkedin: 'https://www.linkedin.com/in/philipp-galliker/',
   //threads: 'https://www.threads.net',
   //instagram: 'https://www.instagram.com',
   //medium: 'https://medium.com/@philippgalliker',
